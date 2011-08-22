f3bf8a30d280ee38d0e87ea1344ca088c5aa0419
eytan <eytan@chartbeat.com>
2011-08-22 19:55:23 -0400

    configure db

--- local_settings.py
+++ local_settings.py
@@ -4,15 +4,15 @@ DEBUG = True
 DATABASES = {
     "default": {
         # "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "sqlite3",
+        "ENGINE": "mysql",
         # DB name or path to database file if using sqlite3.
-        "NAME": "mezzanine.db",
+        "NAME": "bkadin",
         # Not used with sqlite3.
-        "USER": "",
+        "USER": "root",
         # Not used with sqlite3.
         "PASSWORD": "",
         # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
+        "HOST": "localhost",
         # Set to empty string for default. Not used with sqlite3.
         "PORT": "",
     }
