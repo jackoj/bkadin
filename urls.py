35096bbfbe81f49d7e378add25e8cde1ea1b06b4
eytan <eytan85@gmail.com>
2011-08-28 17:57:57 -0400

    fixing filebrowser

--- urls.py
+++ urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
+from mezzanine.conf import settings
+from mezzanine.utils.urls import static_urls
 from mezzanine.core.views import direct_to_template
 
 
@@ -16,13 +18,20 @@ urlpatterns = patterns("mezzanine.generic.views",
     url("^rating/$", "rating", name="rating"),
 )
 
+if getattr(settings, "PACKAGE_NAME_FILEBROWSER") in settings.INSTALLED_APPS:
+    urlpatterns += patterns("",
+        ("^admin/filebrowser/", include("%s.urls" %
+                                        settings.PACKAGE_NAME_FILEBROWSER)),
+        static_urls(settings.FILEBROWSER_URL_FILEBROWSER_MEDIA.strip("/"),
+                    settings.FILEBROWSER_PATH_FILEBROWSER_MEDIA),
+    )
+
 urlpatterns += patterns("",
     ("^admin/", include(admin.site.urls)),
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
     url("^(?P<slug>.*)/$", "ecms.views.topic_page", name="topic_page"),
     ("^", include("mezzanine.urls")),
 )
-
 # Adds ``MEDIA_URL`` to the context.
 handler500 = "mezzanine.core.views.server_error"
 
