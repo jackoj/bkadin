92920f487841d5d8e441bbc596ad19419b2e5f41
eytan <eytan@chartbeat.com>
2011-08-29 23:49:45 -0400

    fix urls messup, by removing topic_page

--- urls.py
+++ urls.py
@@ -6,7 +6,6 @@ from mezzanine.conf import settings
 from mezzanine.utils.urls import static_urls
 from mezzanine.core.views import direct_to_template
 
-
 admin.autodiscover()
 
 # Add the urlpatterns for any custom Django applications here.
@@ -29,9 +28,10 @@ if getattr(settings, "PACKAGE_NAME_FILEBROWSER") in settings.INSTALLED_APPS:
 urlpatterns += patterns("",
     ("^admin/", include(admin.site.urls)),
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
-    url("^(?P<slug>.*)/$", "ecms.views.topic_page", name="topic_page"),
+#    url("^(?P<slug>.*)/$", "ecms.views.topic_page", name="topic_page"),
     ("^", include("mezzanine.urls")),
 )
+
 # Adds ``MEDIA_URL`` to the context.
 handler500 = "mezzanine.core.views.server_error"
 
