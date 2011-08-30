686170263cb79dabee849eb17923b7fed6db6a4f
eytan <eytan@chartbeat.com>
2011-08-29 23:46:44 -0400

    add migrations directory

---
+++ migrations/create_ecms_richtextdata.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `ecms_richtextdata` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `header` varchar(100) NOT NULL,
+    `homepage_header` varchar(100) NOT NULL,
+    `rich_text_page_id` integer NOT NULL
+) CHARSET=utf8
+;
